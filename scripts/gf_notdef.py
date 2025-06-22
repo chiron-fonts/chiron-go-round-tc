1fd4a99d01ce89edb31cbba15ceee2f81596f4fb
Aaron Bell <aaron@sajatypeworks.com>
2025-06-22 05:29:20 -0700

    Update gf_notdef.py

--- scripts/gf_notdef.py
+++ scripts/gf_notdef.py
@@ -9,6 +9,8 @@ for file in SOURCE.glob("*.ufo"):
 	font = Font.open(file)
 	try:
 		font.renameGlyph("cid00000",".notdef")
+		font.renameGlyph("cid41644","uni00A0")
+		font.renameGlyph("cid41639","space")
 	except:
 		print("*** NOTDEF RENAME FAILED ***")
 
