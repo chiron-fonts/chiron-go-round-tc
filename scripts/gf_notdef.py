d51d78227784e097b7f02b73241e32fcfe6517f0
Aaron Bell <aaron@sajatypeworks.com>
2025-06-22 21:55:46 -0700

    Updating build script
    Now corrects two additional bugs and adds the vhea / vmtx tables.

--- scripts/gf_notdef.py
+++ scripts/gf_notdef.py
@@ -7,11 +7,22 @@ SOURCE = Path("/tmp")
 for file in SOURCE.glob("*.ufo"):
 	print ("Processing",file)
 	font = Font.open(file)
-	try:
+	if "390" in str(file):
 		font.renameGlyph("cid00000",".notdef")
-		font.renameGlyph("cid41644","uni00A0")
-		font.renameGlyph("cid41639","space")
-	except:
-		print("*** NOTDEF RENAME FAILED ***")
+	else:
+		try:
+			font.renameGlyph("cid00000",".notdef")
+			font.renameGlyph("cid41644","uni00A0")
+			font.renameGlyph("cid41639","space")
+		except:
+			print ("*RENAME FAILED*")
+
+	font.info.openTypeVheaVertTypoAscender = 500
+	font.info.openTypeVheaVertTypoDescender = -500
+	font.info.openTypeVheaVertTypoLineGap = 500
+	font.info.openTypeNameDesigner = "Ryoko NISHIZUKA 西塚涼子 (kana, bopomofo & ideographs); Paul D. Hunt (Latin, Greek & Cyrillic); Sandoll Communications 산돌커뮤니케이션, Soo-young JANG 장수영 & Joo-yeon KANG 강주연 (hangul elements, letters & syllables); Tamcy (Chinese glyphs modifications & additions)ß"
+
+	for glyph in font:
+		glyph.height = 1000
 
 	font.save(file,overwrite=True)
