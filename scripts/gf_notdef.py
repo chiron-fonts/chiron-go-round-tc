803fb2f0423d5ce9683859cdaa19942896abaf74
Aaron Bell <aaron@sajatypeworks.com>
2025-06-24 15:01:54 -0700

    Fixed a couple things for FB
    - Added glyph height, and vhea info to ensure generation of the vmtx / vhea tables
    - Also added NID 9, which was not being converted.

--- scripts/gf_notdef.py
+++ scripts/gf_notdef.py
@@ -7,20 +7,12 @@ SOURCE = Path("/tmp")
 for file in SOURCE.glob("*.ufo"):
 	print ("Processing",file)
 	font = Font.open(file)
-	if "390" in str(file):
-		font.renameGlyph("cid00000",".notdef")
-	else:
-		try:
-			font.renameGlyph("cid00000",".notdef")
-			font.renameGlyph("cid41644","uni00A0")
-			font.renameGlyph("cid41639","space")
-		except:
-			print ("*RENAME FAILED*")
+	font.renameGlyph("cid00000",".notdef")
 
 	font.info.openTypeVheaVertTypoAscender = 500
 	font.info.openTypeVheaVertTypoDescender = -500
 	font.info.openTypeVheaVertTypoLineGap = 500
-	font.info.openTypeNameDesigner = "Ryoko NISHIZUKA 西塚涼子 (kana, bopomofo & ideographs); Paul D. Hunt (Latin, Greek & Cyrillic); Sandoll Communications 산돌커뮤니케이션, Soo-young JANG 장수영 & Joo-yeon KANG 강주연 (hangul elements, letters & syllables); Tamcy (Chinese glyphs modifications & additions)ß"
+	font.info.openTypeNameDesigner = "Ryoko NISHIZUKA 西塚涼子 (kana, bopomofo & ideographs); Paul D. Hunt (Latin, Greek & Cyrillic); Sandoll Communications 산돌커뮤니케이션, Soo-young JANG 장수영 & Joo-yeon KANG 강주연 (hangul elements, letters & syllables); Tamcy (Chinese glyphs modifications & additions)"
 
 	for glyph in font:
 		glyph.height = 1000
